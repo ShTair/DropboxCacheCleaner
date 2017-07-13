c5978e26da20910d1225aea6e51b0673e673407c
ShTair <git@vsh.jp>
2017-07-13 10:29:22 +0900

    中身の実装

--- DropboxCacheCleaner/Program.cs
+++ DropboxCacheCleaner/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace DropboxCacheCleaner
 {
     class Program
     {
+        private static FileSystemWatcher _fsw;
+
         static void Main(string[] args)
         {
+            _fsw = new FileSystemWatcher(Path.Combine(args[0], ".dropbox.cache"));
+            _fsw.Created += _fsw_Created; ;
+            _fsw.IncludeSubdirectories = true;
+            _fsw.EnableRaisingEvents = true;
+
+            Console.ReadLine();
+        }
+
+        private static void _fsw_Created(object sender, FileSystemEventArgs e)
+        {
+            CheckDelete(e.FullPath);
+        }
+
+        private static void CheckDelete(string path)
+        {
+            if (path.IndexOf("(deleted") != -1)
+            {
+                Console.WriteLine("Delete: " + path);
+                File.Delete(path);
+            }
         }
     }
 }
