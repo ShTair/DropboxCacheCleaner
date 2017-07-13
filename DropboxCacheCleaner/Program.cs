81d7423b1ff85cd8bd0e4b3c2d44ada000194dd6
ShTair <git@vsh.jp>
2017-07-13 10:25:12 +0900

    コンソールアプリに変更

---
+++ DropboxCacheCleaner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxCacheCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
